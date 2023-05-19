57ce188637e7fa9783e1757b24329ee72a8f3d0d
Serg <patron.serg@yandex.ru>
2023-05-19 13:13:16 +0300

    25

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,4 +2,15 @@
 
 // 3, 5 -> 243 (3⁵)
 
-// 2, 4 -> 16
+System.Console.WriteLine("Напишите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Напишите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (int i = 0; i < b; i++)
+{
+    count = count * a;
+}
+Console.WriteLine($"{count} ({a}^{b})");
+
+
